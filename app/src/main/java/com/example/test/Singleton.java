272a20b47a2923679e4732aee9d98e34dc12da78
zhenghanhuang <zhenghanhuang@moneydj.com>
2026-03-28 16:20:56 +0800

    Factory

--- app/src/main/java/com/example/test/Singleton.java
+++ app/src/main/java/com/example/test/Singleton.java
@@ -10,3 +10,26 @@ public class Singleton {
         return mInstance;
     }
 }
+
+interface CellPhone{
+    void createMonitor();
+}
+
+class APhone implements CellPhone{
+
+    @Override
+    public void createMonitor() {
+        System.out.println("製造螢幕！");
+    }
+}
+
+class Factory{
+    APhone mAPhone = new APhone();
+
+    public void createPhone(){
+        mAPhone.createMonitor();
+    }
+}
+
+Factory f = new Factory();
+f.createPhone();

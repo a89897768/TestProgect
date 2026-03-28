1be50c9f18133825c3bb291e387e41e2c6dc96a3
zhenghanhuang <zhenghanhuang@moneydj.com>
2026-03-28 18:21:43 +0800

    原生元件登入_過程1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText mEditText = findViewById(R.id.id_edittext);
+        TextView mTextView = findViewById(R.id.id_textview);
+        mTextView.setOnClickListener(v -> {
+            if(mEditText.equals("1")){
+                Toast.makeText(this,"登入",1);
+            }else{
+                Toast.makeText(this,"登出",1);
+            }
+        });
     }
 }
